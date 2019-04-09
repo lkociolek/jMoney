fac187095927d5e01e447919f90e814544aa29b0
Łukasz Kociołek <lukasz.kociolek@ttpsc.pl>
2019-04-09 14:51:46 +0200

    created maven submodlues project

--- jmoney-backend/src/main/java/dev/kociolek/jmoney/DataLoader.java
+++ jmoney-backend/src/main/java/dev/kociolek/jmoney/DataLoader.java
@@ -1,14 +1,22 @@
 package dev.kociolek.jmoney;
 
+import dev.kociolek.jmoney.domain.Role;
+import dev.kociolek.jmoney.domain.User;
 import dev.kociolek.jmoney.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Component
 public class DataLoader implements ApplicationRunner {
 
+    private final static Role USER = Role.USER;
+    private final static Role ADMIN = Role.ADMIN;
     private UserService service;
 
     @Autowired
@@ -18,29 +26,29 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-//        Set<Role> roles = new HashSet<>(Arrays.asList(Role.ROLE_ADMIN));
-//        User user = new User("admin",
-//                "admin@company.com",
-//                "password",
-//                true,
-//                roles);
-//        service.create(user);
-//
-//        roles = new HashSet<>(Arrays.asList(Role.ROLE_USER));
-//        user = new User("enabled",
-//                "user@company.com",
-//                "password",
-//                true,
-//                roles);
-//        service.create(user);
-//
-//        roles = new HashSet<>(Arrays.asList(Role.ROLE_USER));
-//        user = new User("disabled",
-//                "jkowalski@company.com",
-//                "password",
-//                false,
-//                roles);
-//        service.create(user);
+        Set<Role> roles = new HashSet<>(Arrays.asList(ADMIN));
+        User user = new User("admin",
+                "admin@company.com",
+                "password",
+                true,
+                roles);
+        service.create(user);
+
+        roles = new HashSet<>(Arrays.asList(USER));
+        user = new User("enabled",
+                "user@company.com",
+                "password",
+                true,
+                roles);
+        service.create(user);
+
+        roles = new HashSet<>(Arrays.asList(USER));
+        user = new User("disabled",
+                "jkowalski@company.com",
+                "password",
+                false,
+                roles);
+        service.create(user);
 //        Optional<List<User>> byEmail = service.findByEmail("jkowalski@company.com");
 //        if(byEmail.isPresent()) {
 //            List<User> users = byEmail.get();
